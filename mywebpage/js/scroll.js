7aa055fbb56e572226f67fe8170daaa59439452f
Tommy Tse <tommy921092@msn.com>
2018-10-09 02:49:26 +0800

    scrollspy plugin

--- mywebpage/js/scroll.js
+++ mywebpage/js/scroll.js
@@ -9,19 +9,4 @@ $(document).ready(function() {
     }, 1000 );
     });
 
-        // Active link switching
-    $(window).scroll(function() {
-        var scrollbarLocation = $(this).scrollTop();
-
-        scrollLink.each(function() {
-
-        var sectionOffset = $(this.hash).offset().top - 20;
-
-        if ( sectionOffset <= scrollbarLocation ) {
-            $(this).parent().addClass('active');
-            $(this).parent().siblings().removeClass('active');
-        }
-        })
-
-    })
 });
