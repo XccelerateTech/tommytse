bc00dc3679030c48eab06d339527fa65af282c75
Tommy Tse <tommy921092@msn.com>
2018-11-01 08:23:37 +0800

    add comment

--- Exercise/Day23/D/app.js
+++ Exercise/Day23/D/app.js
@@ -4,9 +4,11 @@ const app = express();
 app.use(bodyParser.json());
 app.post('/', (req, res) => {
     var arr = req.body.reduce((acc, ele)=>acc+ele);
-    res.send(arr.toString());
+    res.send(arr.toString());//need to string for res.send
     console.log(JSON.stringify(arr));
     console.log(arr.toString());
+    console.log(req.body);
+    console.log(arr);
 })
 
 app.listen(8080);
